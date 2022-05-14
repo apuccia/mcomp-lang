35062fc929433c55d42607848f3a099bc7c15df7
apuccia <alessandropuccia97@gmail.com>
2022-05-14 16:41:46 +0200

    added qualifiers for global provided by component

--- mcomp-codegen/lib/linker.ml
+++ mcomp-codegen/lib/linker.ml
@@ -1,5 +1,6 @@
 open Ast
 open Easy_logging
+open Symbol_table
 
 exception LinkingError of string
 
@@ -17,6 +18,7 @@ let raise_linking_error msg =
   raise (LinkingError msg)
 
 let linked_interfaces = Hashtbl.create 0
+let comps_globals = Hashtbl.create 0
 
 let check_connection connection components =
   match connection with
@@ -90,10 +92,17 @@ let rec qualify_component component =
           }
         <@> component.annot
       in
+      let pr =
+        List.filter
+          (fun x -> match x.node with VarDecl _ -> true | _ -> false)
+          cd.definitions
+      in
+      let globals = List.map (fun x -> x.node) pr in
+      Hashtbl.add comps_globals cd.cname globals;
       dbg_link (show_component_decl pp_typ comp);
       comp
 
-and qualify_lv lv cname =
+and qualify_lv lv cname scope =
   match lv.node with
   | AccVar (id1, id2) ->
       let av =
@@ -101,40 +110,49 @@ and qualify_lv lv cname =
           let h = Hashtbl.find linked_interfaces cname in
           let q = Hashtbl.find h (Option.get id1) in
           AccVar (Some q, id2) <@> lv.annot
-        else AccVar (None, id2) <@> lv.annot
+        else
+          try
+            let _ = lookup id2 scope in
+            AccVar (None, id2) <@> lv.annot
+          with NotFoundEntry _ -> AccVar (Some cname, id2) <@> lv.annot
       in
 
       dbg_link (show_lvalue pp_typ av);
       av
   | AccIndex (lv', e) ->
       let ai =
-        AccIndex (qualify_lv lv' cname, qualify_expr e cname) <@> lv.annot
+        AccIndex (qualify_lv lv' cname scope, qualify_expr e cname scope)
+        <@> lv.annot
       in
       dbg_link (show_lvalue pp_typ ai);
       ai
 
-and qualify_expr e cname =
+and qualify_expr e cname scope =
   match e.node with
   | LV lv ->
-      let lv' = LV (qualify_lv lv cname) <@> e.annot in
+      let lv' = LV (qualify_lv lv cname scope) <@> e.annot in
       dbg_link (show_expr pp_typ lv');
       lv'
   | Assign (lv, e) ->
-      let a = Assign (qualify_lv lv cname, qualify_expr e cname) <@> e.annot in
+      let a =
+        Assign (qualify_lv lv cname scope, qualify_expr e cname scope)
+        <@> e.annot
+      in
       dbg_link (show_expr pp_typ a);
       a
   | ILiteral _ | CLiteral _ | BLiteral _ -> e
   | UnaryOp (op, e) ->
-      let uo = UnaryOp (op, qualify_expr e cname) <@> e.annot in
+      let uo = UnaryOp (op, qualify_expr e cname scope) <@> e.annot in
       dbg_link (show_expr pp_typ uo);
       uo
   | Address lv ->
-      let a = Address (qualify_lv lv cname) <@> e.annot in
+      let a = Address (qualify_lv lv cname scope) <@> e.annot in
       dbg_link (show_expr pp_typ a);
       a
   | BinaryOp (op, e1, e2) ->
       let bo =
-        BinaryOp (op, qualify_expr e1 cname, qualify_expr e2 cname) <@> e.annot
+        BinaryOp (op, qualify_expr e1 cname scope, qualify_expr e2 cname scope)
+        <@> e.annot
       in
       dbg_link (show_expr pp_typ bo);
       bo
@@ -145,24 +163,28 @@ and qualify_expr e cname =
           if not (equal_identifier id1 "Prelude") then
             let h = Hashtbl.find linked_interfaces cname in
             let q = Hashtbl.find h id1 in
-            Call (Some q, id2, List.map (fun x -> qualify_expr x cname) el)
+            Call (Some q, id2, List.map (fun x -> qualify_expr x cname scope) el)
             <@> e.annot
           else
-            Call (Some id1, id2, List.map (fun x -> qualify_expr x cname) el)
+            Call
+              (Some id1, id2, List.map (fun x -> qualify_expr x cname scope) el)
             <@> e.annot
         else
-          Call (id1, id2, List.map (fun x -> qualify_expr x cname) el)
+          Call (id1, id2, List.map (fun x -> qualify_expr x cname scope) el)
           <@> e.annot
       in
 
       dbg_link (show_expr pp_typ c);
       c
 
-and qualify_stmt body cname =
+and qualify_stmt body cname scope =
   match body.node with
   | If (e, s1, s2) ->
       let i =
-        If (qualify_expr e cname, qualify_stmt s1 cname, qualify_stmt s2 cname)
+        If
+          ( qualify_expr e cname scope,
+            qualify_stmt s1 cname scope,
+            qualify_stmt s2 cname scope )
         <@> body.annot
       in
 
@@ -170,39 +192,44 @@ and qualify_stmt body cname =
       i
   | While (e, s) ->
       let w =
-        While (qualify_expr e cname, qualify_stmt s cname) <@> body.annot
+        While (qualify_expr e cname scope, qualify_stmt s cname scope)
+        <@> body.annot
       in
 
       dbg_link (show_stmt pp_typ w);
       w
   | Expr e ->
-      let e' = Expr (qualify_expr e cname) <@> body.annot in
+      let e' = Expr (qualify_expr e cname scope) <@> body.annot in
 
       dbg_link (show_stmt pp_typ e');
       e'
   | Return e ->
       let r =
         if Option.is_some e then
-          Return (Some (qualify_expr (Option.get e) cname)) <@> body.annot
+          Return (Some (qualify_expr (Option.get e) cname scope)) <@> body.annot
         else Return e <@> body.annot
       in
 
       dbg_link (show_stmt pp_typ r);
       r
   | Block sl ->
+      let bscope = begin_block scope in
       let b =
-        Block (List.map (fun x -> qualify_stmt_ordec x cname) sl) <@> body.annot
+        Block (List.map (fun x -> qualify_stmt_ordec x cname bscope) sl)
+        <@> body.annot
       in
 
       dbg_link (show_stmt pp_typ b);
       b
   | Skip -> body
 
-and qualify_stmt_ordec stmt cname =
+and qualify_stmt_ordec stmt cname scope =
   match stmt.node with
-  | LocalDecl _ -> stmt
+  | LocalDecl (i, t) ->
+      add_entry i t scope |> ignore;
+      stmt
   | Stmt s ->
-      let s' = Stmt (qualify_stmt s cname) <@> stmt.annot in
+      let s' = Stmt (qualify_stmt s cname scope) <@> stmt.annot in
 
       dbg_link (show_stmtordec pp_typ s');
       s'
@@ -210,19 +237,21 @@ and qualify_stmt_ordec stmt cname =
 and qualify_definition definition cname =
   match definition.node with
   | FunDecl fd ->
-      let fd =
+      let fscope = empty_table () |> begin_block in
+      List.iter (fun x -> match x with (i, t) -> add_entry i t fscope |> ignore) fd.formals;
+      let fd' =
         FunDecl
           {
             rtype = fd.rtype;
             fname = fd.fname;
             formals = fd.formals;
-            body = Some (qualify_stmt (Option.get fd.body) cname);
+            body = Some (qualify_stmt (Option.get fd.body) cname fscope);
           }
         <@> definition.annot
       in
-
-      dbg_link (show_member_decl pp_typ fd);
-      fd
+      end_block fscope |> ignore;
+      dbg_link (show_member_decl pp_typ fd');
+      fd'
   | _ -> definition
 
 let wire_components (CompilationUnit decls : typ compilation_unit) =
